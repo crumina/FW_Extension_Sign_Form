94c671290849098bde3c246b7fc8534113a9e8c4
Sergey Pererva <proger.su@gmail.com>
2019-03-04 17:26:03 +0200

    Update forgot pwd url

--- views/form-login.php
+++ views/form-login.php
@@ -16,15 +16,15 @@ $classes[] = $ext->get_config( 'selectors/form' );
 <div class="title h6"><?php esc_html_e( 'Login to your Account', 'crumina' ); ?></div>
 <form data-handler="<?php echo esc_attr( $ext->get_config( 'actions/signIn' ) ); ?>" class="<?php echo implode( ' ', $classes ); ?>" method="POST" action="<?php echo esc_url( site_url( 'wp-login.php', 'login_post' ) ); ?>">
     <input type="hidden" value="<?php echo wp_create_nonce( 'crumina-sign-form' ); ?>" name="_ajax_nonce" />
-    
+
     <input type="hidden" name="redirect_to" value="<?php echo esc_attr( $redirect_to ); ?>"/>
     <input type="hidden" name="redirect" value="<?php echo esc_attr( $redirect ); ?>"/>
 
     <?php echo apply_filters( 'login_form_top', '' ); ?>
     <?php do_action( 'logy_before_login_fields' ); ?>
-    
+
     <ul class="crumina-sign-form-messages"></ul>
-    
+
     <div class="row">
         <div class="col col-12 col-xl-12 col-lg-12 col-md-12 col-sm-12">
             <div class="form-group label-floating">
@@ -46,13 +46,15 @@ $classes[] = $ext->get_config( 'selectors/form' );
                     </label>
                 </div>
 
-                <a href="<?php echo wp_lostpassword_url(); ?>" class="forgot"><?php esc_html_e( 'Forgot my Password', 'crumina' ); ?></a>
+                <?php $lostpswd  = apply_filters( 'yz_lostpassword_url', wp_lostpassword_url() ); ?>
+
+                <a href="<?php echo esc_url( $lostpswd ); ?>" class="forgot"><?php esc_html_e( 'Forgot my Password', 'crumina' ); ?></a>
             </div>
 
             <button type="submit" class="btn btn-lg btn-primary full-width">
-				<span><?php esc_html_e( 'Login', 'crumina' ); ?></span>
-				<span class="icon-loader"></span>
-			</button>
+                <span><?php esc_html_e( 'Login', 'crumina' ); ?></span>
+                <span class="icon-loader"></span>
+            </button>
 
             <?php echo apply_filters( 'login_form_bottom', '' ); ?>
 
